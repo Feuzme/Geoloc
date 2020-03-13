5b940b468057e64bd414b3b11f76d10bc19f8935
Feuzme <feuzme@gmail.com>
2020-03-13 09:23:01 +0100

    Commit 13032020 Add - DTO Service, Pieces - HTTP Requests for controllers Service, Pieces - ExceptionNotFound classes/advice Service, Pieces MAJ -Entity Chariot

--- src/main/java/snir/rostand/geoloc/Geoloc/controller/PiecesController.java
+++ src/main/java/snir/rostand/geoloc/Geoloc/controller/PiecesController.java
@@ -3,6 +3,7 @@ package snir.rostand.geoloc.Geoloc.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import snir.rostand.geoloc.Geoloc.Payroll.PieceNotFoundException;
+import snir.rostand.geoloc.Geoloc.dto.CreateUpdatePieceDto;
 import snir.rostand.geoloc.Geoloc.entity.Piece;
 import snir.rostand.geoloc.Geoloc.repository.PiecesRepository;
 
@@ -27,5 +28,27 @@ public class PiecesController {
 
     //POST
     @PostMapping("/piece/post")
-    Piece postPieces()
+    Piece postPiece(@RequestBody CreateUpdatePieceDto dto){
+        Piece newPiece = new Piece();
+        newPiece.setNomPiece(dto.getNomPiece());
+        newPiece.setEsp32id(dto.getEsp32id());
+        return piecesRepo.save(newPiece);
+    }
+
+    //PUT
+    @PutMapping("/pieces/put/{idPiece}")
+    Piece putPiece(@PathVariable Integer idPiece, @RequestBody CreateUpdatePieceDto dto){
+        return piecesRepo.findById(idPiece)
+                .map(piece -> {
+                    piece.setNomPiece(dto.getNomPiece());
+                    piece.setEsp32id(dto.getEsp32id());
+                    return piecesRepo.save(piece);
+                }).orElseThrow(()->new PieceNotFoundException(idPiece));
+    }
+
+    //DELETE
+    @DeleteMapping("/pieces/delete/{idPiece}")
+    void deletePiece(@PathVariable Integer idPiece){
+        piecesRepo.deleteById(idPiece);
+    }
 }
--- src/main/java/snir/rostand/geoloc/Geoloc/dto/CreateUpdatePieceDto.java
+++ src/main/java/snir/rostand/geoloc/Geoloc/dto/CreateUpdatePieceDto.java
@@ -1,4 +1,15 @@
 package snir.rostand.geoloc.Geoloc.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateUpdatePieceDto {
+    String nomPiece;
+    Integer Esp32id;
 }
